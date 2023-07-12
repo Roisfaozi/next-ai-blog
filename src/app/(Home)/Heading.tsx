f94f7656d26311fd6ad79672195fdf6d362bbe92
Roisfaozi <roisfaozi55@gmail.com>
2023-07-12 21:16:15 +0700

    create heading component

--- src/app/(Home)/Heading.tsx
+++ src/app/(Home)/Heading.tsx
@@ -3,8 +3,8 @@ type Props = {}
 
 function Heading({ }: Props) {
   return (
-    <div className="border-t border-b border-white justify-center items-center inline-flex w-full">
-      <h1 className="text-white text-center dark:bg-dark font-bold tracking-normal">THE BLOG</h1>
+    <div className="border-t border-b border-white justify-center items-center inline-flex w-full py-4">
+      <h1 className="text-white text-center dark:bg-dark font-bold tracking-normal w-full text-[17vw] sm:text-[18vw] 2xl:text-[15vw]">THE BLOG</h1>
     </div>
   )
 }
