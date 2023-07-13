ff94336c36ccc9a4e4f3c7187c72bea3e459005c
Roisfaozi <roisfaozi55@gmail.com>
2023-07-13 22:23:16 +0700

    finish heading component

--- src/app/(Home)/Heading.tsx
+++ src/app/(Home)/Heading.tsx
@@ -3,8 +3,8 @@ type Props = {}
 
 function Heading({ }: Props) {
   return (
-    <div className="border-t border-b border-white justify-center items-center inline-flex w-full py-4">
-      <h1 className="text-white text-center dark:bg-dark font-bold tracking-normal w-full text-[17vw] sm:text-[18vw] 2xl:text-[15vw]">THE BLOG</h1>
+    <div className="border-t border-b border-dark dark:border-white justify-center items-center inline-flex w-full py-4">
+      <h1 className="dark:text-white text-center text-dark font-bold tracking-normal w-full xl:text-[240px] 2xl:text-[266px] sm:text-[18vw] text-[17vw]">THE BLOG</h1>
     </div>
   )
 }
