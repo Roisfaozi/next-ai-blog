c2348e9a5b414cee9ed95e43ee4558f8873b928d
Roisfaozi <roisfaozi55@gmail.com>
2023-07-16 05:07:55 +0700

    delete unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import Heading from './(Home)/Heading'
 import RecentBlog from './(Home)/RecentBlog'
-const red = 'blue'
 
 function Home() {
-  const bg = `bg-${red}-50`
   return (
     <div className="sm:py-[30px] py-24 flex-col justify-center items-start flex w-full">
       <Heading />
       <RecentBlog />
-      <div className={`w-full h-32 ${bg}`}>
 
-      </div>
     </div>
   )
 }
