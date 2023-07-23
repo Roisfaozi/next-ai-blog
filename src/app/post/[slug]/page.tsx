e7c05f89d8ff187a48c54ff5910ed8b1aaa96fde
Roisfaozi <roisfaozi55@gmail.com>
2023-07-24 00:10:31 +0700

    finish sidebar component

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import SubHeading from '@/src/Components/SubHeading'
+import Sidebar from '../../(PostDetail)/Sidebar'
 
 type Props = {}
 
@@ -6,14 +7,17 @@ function PostDetail({ }: Props) {
   return (
     <div className="gap-8 md:flex-row-reverse md:flex w-full">
       <div className="basis-3/4">
-        <SubHeading level={2} className='text-textdark dark:text-white text-3xl font-semibold text-center'>
-          Post
+        <SubHeading level={1} className='text-textdark dark:text-white text-3xl font-semibold text-center'>
+          Building any open-source projects this weekend?
         </SubHeading>
+
+        <p>A grid system is a design tool used to arrange content on a webpage. It is a series of vertical and
+          horizontal lines that create a matrix of intersecting points, which can be used to align and organize page
+          elements. Grid systems are used to create a consistent look and feel across a website, and can help to
+          make the layout more visually appealing and easier to navigate.</p>
       </div>
       <div className="basis-1/4">
-        <SubHeading level={2} className='text-textdark dark:text-white text-xl font-semibold text-center'>
-          sidebar
-        </SubHeading>
+        <Sidebar />
       </div>
     </div>
   )
